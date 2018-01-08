feae8d0345ddf0efd568940e4f116a1b2a3ea046
Scott Cytacki <scytacki@concord.org>
2018-01-08 16:14:27 -0500

    Need to use a simple animation
    The calibration of the ADC changes with more pixels lit up. So we need to stick with the 'C'
    otherwise we need to recalibrate all of the microbits.

--- source/spiral-animation.cpp
+++ source/spiral-animation.cpp
@@ -1,7 +1,27 @@
 #include "thermoscope.h"
 #include "spiral-animation.h"
 
+void displayAnimationBasic()
+{
+  display.clear();
+  display.image.setPixelValue(0, 0, 255);
+}
+
 void displayAnimation()
+{
+  display.clear();
+  display.print('C');
+}
+
+void displayAnimationEyes()
+{
+  display.clear();
+  // display.print('C');
+  display.image.setPixelValue(1, 0, 255);
+  display.image.setPixelValue(3, 0, 255);
+}
+
+void displayAnimationSpiral()
 {
   int8_t i = 0;
   int8_t bl = 0;
@@ -22,9 +42,13 @@ void displayAnimation()
   // start off with some non initial point
   uint8_t oldA = a + 1;
   uint8_t oldB = b + 1;
+  uint8_t oldOldA = a + 2;
+  uint8_t oldOldB = b + 2;
   while(ble.getGapState().connected) {
-      display.image.setPixelValue(a, b, 128);
-      display.image.setPixelValue(oldA, oldB, 0);
+      display.image.setPixelValue(a, b, 255);
+      display.image.setPixelValue(oldOldA, oldOldB, 0);
+      oldOldA = oldA;
+      oldOldB = oldB;
       oldA = a;
       oldB = b;
       fiber_sleep(200);
